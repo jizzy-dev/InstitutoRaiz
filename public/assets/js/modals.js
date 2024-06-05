7353abccbc4aaa0e5668026176e5c8ae9d1f49bd
Jeziel <141182187+j1zzy19@users.noreply.github.com>
2024-06-05 17:41:35 -0300

    Sidebar menu update

---
+++ public/assets/js/modals.js
@@ -0,0 +1,55 @@
+const modal = document.getElementById('modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+if (openModal) {
+    openModal.addEventListener('click', () => {
+        modal.showModal();
+    })
+}
+if (closeModal) {
+    closeModal.addEventListener('click', () => {
+        modal.close();
+    })
+}
+
+if (modal) {
+    document.addEventListener('DOMContentLoaded', () => {
+        modal.showModal();
+    });
+}
+const sidebar = document.getElementById('sidebar');
+const sidebarBackdrop = document.querySelector('.sidebar::backdrop');
+const openSidebar = document.querySelector('.open-sidebar-profile-button');
+const closeSidebar = document.querySelector('.close-sidebar-button');
+
+if (openSidebar) {
+    openSidebar.addEventListener('click', () => {
+        sidebar.showModal();
+    })
+}
+if (closeSidebar) {
+    closeSidebar.addEventListener('click', () => {
+        sidebar.close();
+    })
+}
+if (sidebar) {
+    sidebar.addEventListener('click', (event) => {
+        if (event.target == sidebar) {
+            sidebar.setAttribute('closing', "");
+            sidebar.addEventListener('animationend', () => {
+                sidebar.removeAttribute('closing');
+                sidebar.close();
+            }, { once: true })
+        }
+    })
+    sidebar.addEventListener('mouseover', (event) => {
+        if (event.target == sidebar) {
+            sidebar.setAttribute('closing', "");
+            sidebar.addEventListener('animationend', () => {
+                sidebar.removeAttribute('closing');
+                sidebar.close();
+            }, { once: true })
+        }
+    })
+}
